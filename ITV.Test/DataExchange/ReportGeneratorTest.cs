2a26e79eb1a6d74dd8b8c8ae7000b986426217bc
Laura <lsp3114@gmail.com>
2026-05-26 19:39:51 +0200

    eliminando test de ReportGeneratorTest

--- ITV.Test/DataExchange/ReportGeneratorTest.cs
+++ ITV.Test/DataExchange/ReportGeneratorTest.cs
@@ -46,41 +46,4 @@ public class ReportGeneratorTest
         }
     }
     
-    [TestFixture]
-    public class CasosInvalidos
-    {
-        private IReportGenerator<Cita> _report = null!;
-        private static readonly DateTime FechaMat = DateTime.Today.AddYears(-1);
-        private static readonly DateTime FechaInsp = DateTime.Today.AddDays(15);
-
-        [SetUp]
-        public void SetUp()
-        {
-            _report = new ReportGenerator();
-        }
-        
-        [TearDown]
-        public void TearDown()
-        {
-            _report = null!;
-        }
-        
-        [Test]
-        public void ExportHtml_DevuelveFailure()
-        {
-            var cita = new Cita(FechaMat, FechaInsp, "1111BBB", "Toyota", "ElMejor", 3.3, Motor.Diesel, "12345678Z");
-            var result = _report.ExportHtml(cita);
-            result.IsFailure.Should().BeTrue();
-            result.Error.Should().BeOfType<ReportError.ReportHtmlError>();
-        }
-        
-        [Test]
-        public void ExportPdf_DevuelveFailure()
-        {
-            var cita = new Cita(FechaMat, FechaInsp, "1111BBB", "Toyota", "ElMejor", 3.3, Motor.Diesel, "12345678Z");
-            var result = _report.ExportPdf(cita);
-            result.IsFailure.Should().BeTrue();
-            result.Error.Should().BeOfType<ReportError.ReportPdfError>();
-        }
-    }
 }
